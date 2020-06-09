5173dcf6075bea3aee754ca9e2374b29c432d676
James Ambrose <galaxy2198@gmail.com>
2020-06-08 21:16:04 -0500

    push before branches

---
+++ beats/heartbeats/grab-heartbeat.ps1
@@ -0,0 +1,1 @@
+wget "https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-7.7.1-windows-x86_64.zip" -OutFile heartbeat.zip
---
+++ clean-up-cluster-unix.sh
@@ -0,0 +1,2 @@
+docker-compose -f elastic-docker-tls.yml down -v
+rm passwords.txt
