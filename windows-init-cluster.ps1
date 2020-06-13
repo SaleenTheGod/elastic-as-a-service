bd338fddb5fefb58fe18e4dca4d190c9201f6296
James Ambrose <galaxy2198@gmail.com>
2020-06-13 08:24:02 -0500

    added better error handling

---
+++ windows-init-cluster.ps1
@@ -0,0 +1,17 @@
+Write-Host "Beginning build out of a 3 node Elasticsearch cluster with a Kibana front end."
+docker-compose -f create-certs.yml run --rm create_certs
+if ($?)
+{
+     docker-compose -f elastic-docker-tls.yml up -d
+     Write-Host "Waiting for Elastic cluster to come online to perform API calls."
+     Start-Sleep -s 30
+     docker exec es01 /bin/bash -c "bin/elasticsearch-setup-passwords auto --batch --url https://es01:9200" >> .\passwords.txt
+     Write-Host 'Please update the value for "ELASTICSEARCH_PASSWORD" in elastic-docker-tls.yml with the "Kibana" password in the newly created "passwords.txt" file.'
+     Read-Host "Please press enter when that value is updated"
+     docker-compose -f elastic-docker-tls.yml stop
+     docker-compose -f elastic-docker-tls.yml up -d
+}
+if (!$?)
+{
+     Write-Host "Failed to start docker containers. Is Docker running with docker-compose installed? https://docs.docker.com/compose/install/"
+}
